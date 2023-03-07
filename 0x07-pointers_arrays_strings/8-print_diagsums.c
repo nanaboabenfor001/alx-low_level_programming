73021c1c6f4511f9e5c97c574102eb64f2f1bc54
root <nanaboabenfor001@gmail.com>
2023-03-06 17:13:32 -0800

    Print sum of diagonalls of a square matric

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,20 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int i, j;
-    int sum1 = 0, sum2 = 0;
+    int i, sum1 = 0, sum2 = 0;
 
+    /* Calculate the sum of the first diagonal */
     for (i = 0; i < size; i++)
     {
-        sum1 += a[i * size + i];
-        sum2 += a[i * size + size - i - 1];
+        sum1 += *(a + i*size + i);
     }
 
+    /* Calculate the sum of the second diagonal */
+    for (i = 0; i < size; i++)
+    {
+        sum2 += *(a + i*size + (size-1-i));
+    }
+
+    /* Print the sums */
     printf("%d, %d\n", sum1, sum2);
 }
