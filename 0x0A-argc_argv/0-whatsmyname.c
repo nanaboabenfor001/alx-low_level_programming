f7dad72564fc6774f3bd21f6858798362d423214
root <nanaboabenfor001@gmail.com>
2023-03-14 16:38:05 -0700

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments passed to the program
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
