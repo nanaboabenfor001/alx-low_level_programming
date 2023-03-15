0907ca28bdf0022952a60c6eacc25546e76964d2
root <nanaboabenfor001@gmail.com>
2023-03-15 14:27:46 -0700

    Print an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,39 +1,28 @@
-#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
-void simple_print_buffer(char *buffer, unsigned int size)
+/**
+ * create_array - Creates an array of chars, and initializes it with a specific char.
+ * @size: The size of the array to be created.
+ * @c: The char to initialize the array with.
+ *
+ * Return: If size = 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
+ */
+char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
+	char *arr;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
+	if (size == 0)
+		return (NULL);
 
-int main(void)
-{
-    char *buffer;
+	arr = malloc(size * sizeof(char));
 
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
-    return (0);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
 }
