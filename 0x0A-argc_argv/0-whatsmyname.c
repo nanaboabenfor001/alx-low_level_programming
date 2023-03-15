4c598dbec95f3d693f3388645af96f3fbc05fc19
root <nanaboabenfor001@gmail.com>
2023-03-15 15:48:43 -0700

    Print name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/* Declare the __progname variable */
-extern char *__progname;
 
 /**
- * main - Entry point
- *
- * @argc: The number of arguments passed to the program
- * @argv: An array of strings containing the arguments passed to the program
+ * main - prints the name of the program
+ * @argc: the number of command-line arguments
+ * @argv: an array of strings containing the arguments
  *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-    /* Print the program name using the __progname variable */
-    printf("%s\n", __progname);
-
-    return (EXIT_SUCCESS);
+        printf("%s\n", argv[0]);
+        return (0);
 }
