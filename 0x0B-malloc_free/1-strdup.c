901b623f92dd324127e6704d2fa2e4d9bb02e0af
root <nanaboabenfor001@gmail.com>
2023-03-15 14:52:13 -0700

    Return pointer to new memory location

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,10 +4,10 @@
 /**
  * _strdup - returns a pointer to a newly allocated space in memory
  * which contains a copy of the string given as a parameter.
- * @str: pointer to the string to copy
+ * @str: string to be duplicated
  *
- * Return: On success, returns a pointer to the duplicated string.
- * On failure, returns NULL.
+ * Return: pointer to duplicated string or NULL if str is NULL or
+ * if insufficient memory was available
  */
 char *_strdup(char *str)
 {
@@ -17,14 +17,17 @@ char *_strdup(char *str)
         if (str == NULL)
                 return (NULL);
 
+        /* find length of string */
         while (str[len] != '\0')
                 len++;
 
+        /* allocate memory for duplicate string */
         dup_str = malloc(sizeof(char) * (len + 1));
 
         if (dup_str == NULL)
                 return (NULL);
 
+        /* copy string to new memory location */
         for (i = 0; i <= len; i++)
                 dup_str[i] = str[i];
 
