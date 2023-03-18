76106e171a5ff7ddf7a3b1d9a7454a9d6b5bc80a
root <nanaboabenfor001@gmail.com>
2023-03-17 18:56:54 -0700

    Fixed 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,39 +1,48 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * _realloc - Reallocate memory block using malloc and free
- * @ptr: Pointer to the memory previously allocated with a call to malloc
- * @old_size: Size, in bytes, of the allocated space for ptr
- * @new_size: New size, in bytes, of the new memory block
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: Pointer to the memory previously allocated with a call to malloc.
+ * @old_size: Size, in bytes, of the allocated space for ptr.
+ * @new_size: New size, in bytes, of the new memory block.
  *
  * Return: Pointer to the newly allocated memory block.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_ptr, *old_ptr;
-	unsigned int i;
+    void *new_ptr;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-			return (NULL);
-		return (new_ptr);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+    if (new_size == old_size)
+        return (ptr);
 
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-		return (NULL);
+    if (ptr == NULL)
+    {
+        new_ptr = malloc(new_size);
+        if (new_ptr == NULL)
+            return (NULL);
+        return (new_ptr);
+    }
 
-	old_ptr = ptr;
-	for (i = 0; i < old_size && i < new_size; i++)
-		new_ptr[i] = old_ptr[i];
+    if (new_size == 0)
+    {
+        free(ptr);
+        return (NULL);
+    }
 
-	free(ptr);
-	return (new_ptr);
+    new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
+        return (NULL);
+
+    if (new_size < old_size)
+        memcpy(new_ptr, ptr, new_size);
+    else
+        memcpy(new_ptr, ptr, old_size);
+
+    free(ptr);
+
+    return (new_ptr);
 }
