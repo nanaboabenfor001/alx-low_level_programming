75b5d70981b9cef0e29ca0dc7e08f73155e684f8
root <nanaboabenfor001@gmail.com>
2023-03-21 15:58:39 -0700

    rename struct dog to dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,11 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
