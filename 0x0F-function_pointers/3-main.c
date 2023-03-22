2083636195295f2bbdbd3705c23e55028de42958
root <nanaboabenfor001@gmail.com>
2023-03-22 15:18:26 -0700

    Perform simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,42 @@
+#include <stdio.h>
 #include "3-calc.h"
 
 /**
- * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
+ * main - performs simple arithmetic operations
+ * @argc: number of arguments passed
+ * @argv: pointer to array of arguments passed
  *
- * Return: Always 0
+ * Return: 0 on success, 98, 99, or 100 on error
  */
 int main(int argc, char *argv[])
 {
-	int a, b, result;
-	int (*op_func)(int, int);
+	int num1, num2, result;
+	int (*f)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	f = get_op_func(argv[2]);
 
-	op_func = get_op_func(argv[2]);
-
-	if (op_func == NULL)
+	if (f == NULL)
 	{
 		printf("Error\n");
-		exit(99);
+		return (99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
-		exit(100);
+		return (100);
 	}
 
-	result = op_func(a, b);
+	result = f(num1, num2);
+
 	printf("%d\n", result);
 
 	return (0);
