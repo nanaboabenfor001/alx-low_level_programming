6659928a090393ea972827c6d96500ddec460a78
root <nanaboabenfor001@gmail.com>
2023-04-03 16:09:21 -0700

    This functions frees a list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,39 +2,19 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to a pointer to the first node in the list
- * @n: integer to be stored in the new node
+ * free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the listint_t list.
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: void
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+void free_listint(listint_t *head)
 {
-    listint_t *new_node, *current_node;
+    listint_t *temp;
 
-    /* allocate memory for the new node */
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    /* set the value of the new node */
-    new_node->n = n;
-    new_node->next = NULL;
-
-    /* if the list is empty, make the new node the first node */
-    if (*head == NULL)
+    while (head != NULL)
     {
-        *head = new_node;
-        return (new_node);
+        temp = head;
+        head = head->next;
+        free(temp);
     }
-
-    /* traverse the list to find the last node */
-    current_node = *head;
-    while (current_node->next != NULL)
-        current_node = current_node->next;
-
-    /* add the new node to the end of the list */
-    current_node->next = new_node;
-
-    return (new_node);
 }
